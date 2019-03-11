e8a00173a3a878c55d0200845c42a39c2a3fefdd
Iris Epple <46895227+iridecent@users.noreply.github.com>
2019-03-11 13:00:32 +0100

    Add acciracy and perplexity

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ import os, sys
 import argparse
 import numpy as np
 import pandas as pd
-import pickle
 from sklearn.linear_model import LogisticRegression
+from joblib import dump, load
+from statistics import mean
 
 # test.py -- Don't forget to put a reasonable amount code comments
 # in so that we better understand what you're doing when we grade!
@@ -24,5 +25,31 @@ print("Loading model from file {}.".format(args.modelfile))
 
 print("Testing {}-gram model.".format(args.ngram))
 
-print("Accuracy is ...")
-print("Perplexity is...")
+
+model = load(args.modelfile+".joblib")
+data = pd.read_csv(args.datafile, header=None)
+classes = data[data.columns[-1]].to_list()
+vectors = data.iloc[:, :-1]
+
+accuracy = model.score(vectors, classes)
+
+
+def calculate_perplexity(model, vectors):
+    log = model.predict_log_proba(vectors)
+    prob = model.predict_proba(vectors)
+    all_entropy = []
+    for i in range(len(log)):
+        entropy_vect = 0
+        for j in range(len(log[i])):
+            entropy_vect += (-1*prob[i][j]*log[i][j])
+        all_entropy.append(entropy_vect)
+    entropy = mean(all_entropy)
+    perplexity = 2 ** entropy
+    return perplexity
+
+
+perplexity = calculate_perplexity(model, vectors)
+
+
+print("Accuracy is: {}".format(accuracy))
+print("Perplexity is: {}".format(perplexity))
